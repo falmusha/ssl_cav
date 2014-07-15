b961cafbfe4aaeaf19088ef7cef7449648b685c2
Bilal <bilaljq10@gmail.com>
2014-07-15 18:17:50 -0400

    starting to look at function hijack example

--- src/cav.c
+++ src/cav.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include "cav.h"
+#include <dlfcn.h>
 
 typedef int (*orig_SSL_get_verify_result_f_type)(const SSL *ssl);
 typedef int (*orig_do_handshake_f_type)(SSL *s);
@@ -16,6 +18,7 @@ int SSL_do_handshake(SSL *s) {
 
 int SSL_connect(SSL *s) {
   printf("Hijacked SSL_connect\n");
+  orig_SSL_connect_f_type orig_SSL_connect;
   return 0;
 }
 
