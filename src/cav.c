10860adb83f7f401f9d95b36ae2b35fdc3dc983e
Bilal <bilaljq10@gmail.com>
2014-07-15 18:43:36 -0400

    starting to look at function hijack example

--- src/cav.c
+++ src/cav.c
@@ -10,7 +10,7 @@ long SSL_get_verify_result(const SSL *ssl) {
   printf("Hijacked SSL_get_verify_result\n");
   orig_SSL_get_verify_result_f_type orig_SSL_get_verify_result;
   orig_SSL_get_verify_result = (orig_SSL_get_verify_result_f_type)dlsym(RTLD_NEXT,"SSL_get_verify_result");
-  orig_SSL_get_verify_result(ssl);
+  return orig_SSL_get_verify_result(ssl);
   return 0;
 }
 
@@ -18,7 +18,7 @@ int SSL_do_handshake(SSL *s) {
   printf("Hijacked SSL_do_handshake\n");
   orig_do_handshake_f_type orig_do_handshake;
   orig_do_handshake = (orig_do_handshake_f_type)dlsym(RTLD_NEXT,"SSL_do_handshake");
-  SSL_do_handshake(s);
+  return SSL_do_handshake(s);
   return -1;
 }
 
@@ -26,7 +26,7 @@ int SSL_connect(SSL *s) {
   printf("Hijacked SSL_connect\n");
   orig_SSL_connect_f_type orig_SSL_connect;
   orig_SSL_connect = (orig_SSL_connect_f_type)dlsym(RTLD_NEXT,"SSL_connect");
-  orig_SSL_connect(s);
+  return orig_SSL_connect(s);
   return 0;
 }
 
