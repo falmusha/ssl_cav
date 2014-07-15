795051be20b0c875d50dc6161e3489431171d7cc
Bilal <bilaljq10@gmail.com>
2014-07-15 18:53:00 -0400

    starting to look at function hijack example

--- src/cav.c
+++ src/cav.c
@@ -2,31 +2,47 @@
 #include "cav.h"
 #include <dlfcn.h>
 
+//These typedefs just point to aliases with function types and arguments identical to the functions being hijacked.
 typedef long (*orig_SSL_get_verify_result_f_type)(const SSL *ssl);
 typedef int (*orig_do_handshake_f_type)(SSL *s);
 typedef int (*orig_SSL_connect_f_type)(SSL *s);
 
 long SSL_get_verify_result(const SSL *ssl) {
+  //Our hijacked implementation
   printf("Hijacked SSL_get_verify_result\n");
+
+  //Call the original SSL_Connect function from the SSL library. RTLD_NEXT simply looks at the
+  //next library with a definition for SSL_Connect 
   orig_SSL_get_verify_result_f_type orig_SSL_get_verify_result;
   orig_SSL_get_verify_result = (orig_SSL_get_verify_result_f_type)dlsym(RTLD_NEXT,"SSL_get_verify_result");
   return orig_SSL_get_verify_result(ssl);
+
   return 0;
 }
 
 int SSL_do_handshake(SSL *s) {
+  //our hijacked implementation
   printf("Hijacked SSL_do_handshake\n");
+
+  //Call the original SSL_do_handshke function from the SSL library. RTLD_NEXT simply looks at the
+  //next library with a definition for SSL_do_handshake 
   orig_do_handshake_f_type orig_do_handshake;
   orig_do_handshake = (orig_do_handshake_f_type)dlsym(RTLD_NEXT,"SSL_do_handshake");
   return SSL_do_handshake(s);
+
   return -1;
 }
 
-int SSL_connect(SSL *s) {
+int SSL_connect(SSL *s) {  
+  //our hijacked implementation
   printf("Hijacked SSL_connect\n");
+  
+  //Call the original SSL_Connect function from the SSL library. RTLD_NEXT simply looks at the
+  //next library with a definition for SSL_Connect
   orig_SSL_connect_f_type orig_SSL_connect;
   orig_SSL_connect = (orig_SSL_connect_f_type)dlsym(RTLD_NEXT,"SSL_connect");
   return orig_SSL_connect(s);
+
   return 0;
 }
 
